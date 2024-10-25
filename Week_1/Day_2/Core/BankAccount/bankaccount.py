ba343bebd1656b643d0ffab6d0b837d3cb4c1a3b
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-25 16:01:43 +0100

    BankAccount

--- Week_1/Day_2/Core/BankAccount/bankaccount.py
+++ Week_1/Day_2/Core/BankAccount/bankaccount.py
@@ -1,40 +1,32 @@
 class BankAccount:
-    def __init__(self, int_rate=0.01, balance=0):
-        self.int_rate = int_rate
-        self.balance = balance
-
+    int_rate = 0.01 
+    balance = 0
+    def init(self, int_rate, balance): 
+        self.int_rate= int_rate 
+        self.balance =balance
     def deposit(self, amount):
         self.balance += amount
-        return self  
-
+        return self
     def withdraw(self, amount):
-        if amount <= self.balance:
+        if (self.balance - amount) > 0:
             self.balance -= amount
         else:
-            print("Insufficient funds: Charging a $5 fee")
-            self.balance -= 5 
-        return self  
+            print("insufficient funds : Charging a $5 fee")
+            self.balance=self.balance-5
+        return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance:.2f}")
-        return self  
-
+        print(f"Balance : ${self.balance}")
+        return self 
     def yield_interest(self):
-        if self.balance > 0:
-            self.balance += self.balance * self.int_rate
-        return self    
-
-    @classmethod
-    def print_all_accounts_info(cls, accounts):
-        for account in accounts:
-            account.display_account_info()
-
-
-account1 = BankAccount(int_rate=0.05)
-account2 = BankAccount(int_rate=0.03)
-
-account1.deposit(100).deposit(50).withdraw(30).yield_interest().display_account_info()
-
-account2.deposit(200).deposit(100).withdraw(50).withdraw(30).withdraw(20).withdraw(10).yield_interest().display_account_info()
-
-BankAccount.print_all_accounts_info([account1, account2])
+        if self.balance > 0 :
+            self.balance = self.balance+(self.balance * self.int_rate)
+        return self 
+user_1 = BankAccount(0.3,300)
+user_2= BankAccount(0.4,100)
+user_1.deposit(50).deposit(100).deposit(30).withdraw(300).yield_interest().display_account_info()
+user_2.deposit(20).deposit(40).withdraw(50).withdraw(100).withdraw(60).withdraw(100).yield_interest().display_account_info()
+0 commit comments
+Comments
+0
+ (0)
