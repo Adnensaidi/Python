5b91f9ec7e42ba203a88c7bc3a85e99e7187940b
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-25 15:58:40 +0100

    User

--- Week_1/Day_2/Practice/User/user.py
+++ Week_1/Day_2/Practice/User/user.py
@@ -1,5 +1,5 @@
-class User:
-    def __init__(self, first_name, last_name, email, age):
+class user:
+    def init(self, first_name, last_name,email,age):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -8,46 +8,97 @@ class User:
         self.gold_card_points = 0
 
     def display_info(self):
-        print(f"First Name: {self.first_name}")
-        print(f"Last Name: {self.last_name}")
-        print(f"Email: {self.email}")
-        print(f"Age: {self.age}")
-        print(f"Rewards Member: {self.is_rewards_member}")
-        print(f"Gold Card Points: {self.gold_card_points}")
+        print(f"name: {self.first_name} {self.last_name}")
+        print(f"email: {self.email}")
+        print(f"age : {self.age}")
+        print(f"reward member : {self.is_rewards_member}")
+        print(f"gold card points : {self.gold_card_points}")
 
     def enroll(self):
         if self.is_rewards_member:
-            print("User already a member.")
+            print(f"{self.first_name}user is alrady an the members")
             return False
         else:
             self.is_rewards_member = True
             self.gold_card_points = 200
+            print(f"{self.first_name} has been enrolled in the rewards program")
             return True
 
     def spend_points(self, amount):
-        if amount > self.gold_card_points:
-            print("Not enough points to spend.")
-            return False
-        else:
+        if self.gold_card_points >= amount:
             self.gold_card_points -= amount
-            return True
+            print(f"{self.first_name} spent {amount} poitns")
+        else:
+            print(f"{self.first_name} does not have enough points")
 
 
-if __name__ == "__main__":
-    user1 = User("John", "Doe", "john@example.com", 30)
-    user1.display_info()
+user1 = user("John","Doe", "johndoe@efel.com",20)
+user2 = user("Zayn", "Jorden", "zayn@efel.com", 40)
+user3 = user("Liam", "Paine", "liam@nefel.com", 31)
 
-    user1.enroll()
+user1.display_info()
 
-    user2 = User("Jane", "Smith", "jane@example.com", 28)
-    user3 = User("Bob", "Brown", "bob@example.com", 25)
+user1.enroll()
+user2.enroll()
 
-    user1.spend_points(50)
+user1.spend_points(50)
+user2.spend_points(80)
 
-    user2.enroll()
+user1.display_info()
+user2.display_info()
+user3.display_info()
 
-    user2.spend_points(80)
+user1.enroll()
 
-    user1.display_info()
-    user2.display_info()
-    user3.display_info()
+user3.spend_points(40)
+methode
+class User:
+    def init(self, first_name, last_name,email,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f"name: {self.first_name} {self.last_name}")
+        print(f"email: {self.email}")
+        print(f"age : {self.age}")
+        print(f"reward member : {self.is_rewards_member}")
+        print(f"gold card points : {self.gold_card_points}")
+        return self
+
+
+
+    def enroll(self):
+        if  self.is_rewards_member == True:
+            self.gold_card_points == 200
+            print(f"{self.first_name} has been enrolled as a rewards member.")
+        else:
+            print(f"{self.first_name} is already a rewards member.")
+        return self
+
+
+
+    def spend_points(self, amount):
+        if self.gold_card_points >= amount:
+            self.gold_card_points -= amount
+            print(f"{self.first_name} spent {amount} poitns")
+        else:
+            print(f"{self.first_name} does not have enough points")
+        return self
+
+
+user1 = User("John","Doe", "johndoe@efel.com",20)
+user2 = User("Zayn", "Jorden", "zayn@efel.com", 40)
+user3 = User("Liam", "Paine", "liam@nefel.com", 31)
+
+
+user1.display_info().enroll().spend_points(50).dispaly_info()     
+user1.display_info().enroll().spend_points(50).display_info()
+
+
+
+
+user2.display_info().enroll().spend_points(50).display_info()
