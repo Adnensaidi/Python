c049a254a06038ac09dc865a88abde4593689d75
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-29 14:57:14 +0100

    Users

---
+++ MySQL/Week_2/Day_2/Practice/Users.sql
@@ -0,0 +1,7 @@
+SELECT * FROM users ;
+INSERT INTO users (first_name,last_name,email) VALUES ("ali","ben","ali@gmail.com"),("massoud","benmassoud","massoud@gmail.com"),("dhaw","bendhaw","dhaw@gmail.com");
+SELECT * FROM users WHERE email="ali@gmail.com" ; 
+SELECT * FROM users WHERE id = 3 ; 
+UPDATE users SET last_name = "pancakes" WHERE id=3 ;
+DELETE FROM users WHERE id = 2 ;
+SELECT first_name FROM users ORDER BY first_name;
