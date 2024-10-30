84efa0fd37025afdb4e76a240e1b4890240c052b
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-30 13:17:40 +0100

    Books

--- MySQL/Week_2/Day_2/Practice/Users/Users.sql
+++ MySQL/Week_2/Day_2/Practice/Users/Users.sql
@@ -5,3 +5,4 @@ SELECT * FROM users WHERE id = 3 ;
 UPDATE users SET last_name = "pancakes" WHERE id=3 ;
 DELETE FROM users WHERE id = 2 ;
 SELECT first_name FROM users ORDER BY first_name;
+booksbooks
